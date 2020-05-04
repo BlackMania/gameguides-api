56f188f5b3de9c75e60e491b831918bf97e7a6bf
BlackMania <BlackMania@users.noreply.github.com>
2020-05-04 15:24:05 +0200

    Fixed bugs not decoding the correct payload

--- src/main/java/com/gameguides/api/services/utils/JWTDecoder.java
+++ src/main/java/com/gameguides/api/services/utils/JWTDecoder.java
@@ -1,4 +1,13 @@
 package com.gameguides.api.services.utils;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.json.JSONObject;
+
 public class JWTDecoder {
+    public static JSONObject decodeJWTPayLoad(String token)
+    {
+        String payload = token.split("\\.")[1];
+        byte[] byteArray = Base64.decodeBase64(payload.getBytes());
+        return new JSONObject(new String(byteArray));
+    }
 }
